452a685ba6ff7510ea88312597d195ab82c66b9d
John Dunning <fw@johndunning.com>
2023-01-15 23:27:22 -0800

    Update to latest deps
    Set preview port to 3003 using a CLI flag, as the config setting doesn't seem to work.
    Don't open a tab by default when npm start runs.
    Remove the .prettierrc nonsense.

--- remotion.config.js
+++ remotion.config.js
@@ -1,7 +1,5 @@
-// All configuration options: https://remotion.dev/docs/config
-// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
-// ! The configuration file does only apply if you render via the CLI !
-
 import { Config } from "remotion";
 
+Config.Bundling.setPort(3003);
 Config.Rendering.setImageFormat("jpeg");
+Config.Preview.setShouldOpenBrowser(false);
